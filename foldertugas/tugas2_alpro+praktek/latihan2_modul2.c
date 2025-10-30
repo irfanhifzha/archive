0ed541d654e9a0f7d3e1160b5664edbde5386b32
irfanhifzha <irfan.ulya@digitalbdg.ac.id>
2025-10-30 17:48:45 +0700

    yes

--- foldertugas/tugas2_alpro+praktek/latihan2_modul2.c
+++ foldertugas/tugas2_alpro+praktek/latihan2_modul2.c
@@ -10,17 +10,18 @@ int main(){
     int jumlah_input;
     printf("Masukkan jumlah mahasiswa yang ingin di-input: "); scanf("%d", &jumlah_input);
     list_mahasiswa mhs[jumlah_input];
+    list_mahasiswa *pointer1 = mhs;
 
     for (int i=0; i<jumlah_input; i++){
-        printf("\nMasukkan data mahasiswa ke-%d\n", i+1);
-        printf("Input Nama: "); scanf("%s", mhs[i].nama);
-        printf("Input NIM: "); scanf("%s", mhs[i].nim);
-        printf("Input IPK: "); scanf("%f", &mhs[i].ipk);
+        printf("\nMasukkan data mahasiswa ke-%d\n", i++);
+        printf("Input Nama: "); scanf("%s", pointer1->nama);
+        printf("Input NIM: "); scanf("%s", pointer1->nim);
+        printf("Input IPK: "); scanf("%f", &(*pointer1).ipk);
     }
 
     printf("\nData Mahasiswa:\n");
     for (int a=0; a<jumlah_input; a++){
-        printf("%s | %s | %.2f\n", mhs[a].nama, mhs[a].nim, mhs[a].ipk);
+        printf("%s | %s | %.2f\n", pointer1->nama, pointer1->nim, pointer1->ipk);
     }
     return 0;
 }
