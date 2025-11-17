f8e0c1c571ee4189d52beca8c7e07a072a2db49d
irfanhifzha <irfan.ulya@digitalbdg.ac.id>
2025-11-17 19:53:09 +0700

    fix submit

--- foldertugas/tugas2_alpro+praktek/241010009_IrfanHifzha_menghitung_jumlah_huruf_a.c
+++ foldertugas/tugas2_alpro+praktek/241010009_IrfanHifzha_menghitung_jumlah_huruf_a.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
 int counta_iteratif(char word[]){
-    int count = 0;
+    int count = 0;    // deklarasi count akan +1 untuk space complexity & +1 time complexity.
 
-    for (int i=0; word[i] !='\0'; i++){
+    for (int i=0; word[i] !='\0'; i++){  // deklarasi i konstan +1 time complexity & +1 space complexity, i++ dan membandingkan hingga kata habis +n time complexity sebanyak loop i.
         if (word[i]== 'a'){
             printf("index %d adalah a\n", i);
-            count++;
+            count++;    // proses ini dilakukan sebanyak loop, maka time complexity +n.
         }
-    } return count;
+    } return count;  // proses ini konstan maka +1 time complexity.
 }
 
     // ^^
-    // Time complexity: O(n)
-    // Space complexity: O(1)
+    // Time complexity: O(1+n*1) > O(n)
+    // Space complexity: O(2) > O(1)
 
 int counta_rekursif(char word[]){
-    if (word[0] == '\0') return 0;
-    int count = (word[0] == 'a') ? 1 : 0;
+    if (word[0] == '\0') return 0;  // deklarasi konstan +1 time complexity & +1 space complexity.
+    int count = (word[0] == 'a') ? 1 : 0; // ada proses penggantian variable, +1 time complexity & +1 space complexity.
 
-    return count + counta_rekursif(word+1);
+    return count + counta_rekursif(word+1);  // proses rekursif, +1 time complexity & +1 space complexity, TAPI sebanyak n.. jadi +n time complexity & +n space complexity.
 }
 
     // ^^
-    // Time complexity: O(n)
-    // Space complexity: O(n)
+    // Time complexity: O(3n) > O(n)
+    // Space complexity: O(3n) > O(n)
 
 int main(){
     char kata[50];
@@ -41,3 +41,4 @@ int main(){
     return 0;
 
 }
+
